339e7d1b4abcd7fb83c410149ba5e436686f9948
Jue Ning <juening@gmail.com>
2020-11-28 23:45:27 -0500

    fetching books and single book from mongodb

---
+++ backend/routes/bookRoutes.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+const router = express.Router();
+import Book from '../models/bookModel.js';
+
+
+
+router.get('/', asyncHandler( async (req, res) => {
+    const books = await Book.find({});
+    res.json(books);
+})
+);
+
+router.get('/:id', asyncHandler( async (req, res) => {
+    const book = await Book.findById(req.params.id);
+    if(book) {
+        res.json(book)
+    } else {
+        res.status(404).json({message:'Book not found.'})
+    }
+})
+)
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js'
-import books from './data/books.js';
+
+import bookRoutes from './routes/bookRoutes.js';
 
 dotenv.config();
 
@@ -14,16 +15,9 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('API is running...')
-})
-
-app.get('/api/books', (req, res)=> {
-    res.json(books)
 });
 
-app.get('/api/book/:id', (req, res) => {
-    const book = books.find(b =>b._id === req.params.id);
-    res.json(book);
-});
+app.use('/api/books', bookRoutes)
 
 app.listen(PORT, function(){
     console.log(`App is running in ${process.env.NODE_ENV} on ${PORT}`.green.bold)
