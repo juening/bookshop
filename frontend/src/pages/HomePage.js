866dc13b887a87259a8ff187aa0878299cc91038
Jue Ning <juening@gmail.com>
2020-11-29 22:14:31 -0500

    added Message and Loader component

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -4,10 +4,10 @@ import {Row, Col} from 'react-bootstrap';
 
 import {fetchBooks } from '../actions/bookActions'
 import Book from '../components/Book';
-
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 
 const HomePage = () => {
-
     const dispatch = useDispatch();    
 
     const bookList = useSelector(state => state.bookList);
@@ -21,7 +21,7 @@ const HomePage = () => {
         <>
             <h1>Bestsellers</h1>
             {
-                loading? <h3>Loading...</h3> :error? <h3>{error}</h3>: (<Row>
+                loading? <Loader /> :error? <Message variant='danger'>{error}</Message> : (<Row>
                 {books && books.map(book => (
                     <Col key={book._id} sm={12} md={6} lg={4} xl={3}>
                         <Book book={book} />
