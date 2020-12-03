196b6019e8a1ef63191506295a3a6401d27cdb06
Jue Ning <juening@gmail.com>
2020-12-02 23:26:07 -0500

    backend user authetication with jwt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema({
     name:{
@@ -20,6 +21,11 @@ const userSchema = mongoose.Schema({
         default:false
     }
 }, {timestamps:true});
+
+userSchema.methods.matchPassword =async function(enteredPassword)  {
+    console.log('hello',this.password);
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 
 const User = mongoose.model('User', userSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js'
 
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import bookRoutes from './routes/bookRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -14,11 +15,14 @@ const PORT =  process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 });
 
 app.use('/api/books', bookRoutes);
+app.use('/api/user', userRoutes)
 
 app.use(notFound);
 
