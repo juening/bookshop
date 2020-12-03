56b11dc50c3166e354a242e0a4f4ce59236b4b6e
Jue Ning <juening@gmail.com>
2020-12-03 13:59:23 -0500

    user register at backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -34,4 +34,32 @@ export const getUserProfile = asyncHandler(async (req, res) => {
         res.status(404);
         throw new Error('User not found')
     }
+});
+
+export const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body;
+
+    const userExists = await User.findOne({email});
+
+    if(userExists) {
+        res.status(401);
+        throw new Error('Email already used.')
+    }
+
+    const user = await User.create({
+        email, password, name
+    });
+
+    if(user) {
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            isAdmin:user.isAdmin,
+            token:generateToken(user._id)
+        })
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data.')
+    }
 })
