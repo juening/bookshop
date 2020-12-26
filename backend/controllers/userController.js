9f3fb672878f1ee20a9ca60dcdcce0d568ee2087
Jue Ning <juening@gmail.com>
2020-12-25 23:23:59 -0500

    updated get user by id and update user endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -94,7 +94,7 @@ export const getUsers = asyncHandler(async (req, res) => {
 });
 
 export const deleteUser = asyncHandler(async (req, res) => {
-    const user = User.findById(req.params.id);
+    const user = await User.findById(req.params.id);
 
     if(user) {
         await user.deleteOne();
@@ -104,3 +104,36 @@ export const deleteUser = asyncHandler(async (req, res) => {
         throw new Error('User not found.')
     }
 });
+
+export const getUserById = asyncHandler(async (req, res) => {
+    const user =await User.findById(req.params.id).select('-password');
+
+    if(user) {
+        res.json(user);
+    } else {
+        res.status(404);
+        throw new Error('User not found.')
+    }
+});
+
+export const updateUser = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+
+    if(user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        user.isAdmin = req.body.isAdmin;
+
+        const updatedUser =await  user.save();
+        res.json({
+            _id: updatedUser._id,
+            name:updatedUser.name,
+            email:updatedUser.email,
+            isAdmin:updatedUser.isAdmin
+        })
+    }else {
+        res.status(404);
+        throw new Error('User not found')
+    }
+
+})
