7d096efd406452a4674e8e550c19bf7982f053ba
Jue Ning <juening@gmail.com>
2020-12-27 23:19:47 -0500

    fixed userupdate loop bug by reset the success in updateReducer

--- frontend/src/reducers/userUpdateReducer.js
+++ frontend/src/reducers/userUpdateReducer.js
@@ -1,4 +1,4 @@
-import { USER_UPDATE_FAIL, USER_UPDATE_REMOVE_ALERT, USER_UPDATE_REQUEST, USER_UPDATE_SUCCESS } from '../constants/actionTypes';
+import { USER_UPDATE_FAIL, USER_UPDATE_REMOVE_ALERT, USER_UPDATE_REQUEST, USER_UPDATE_RESET, USER_UPDATE_SUCCESS } from '../constants/actionTypes';
 
 const userUpdateReducer = (state={loading:false, error:null, success:false, }, action) => {
     switch(action.type) {
@@ -13,6 +13,9 @@ const userUpdateReducer = (state={loading:false, error:null, success:false, }, a
 
         case USER_UPDATE_REMOVE_ALERT:
             return {...state, error:null};
+
+        case USER_UPDATE_RESET:
+            return {...state, success:false};
 
         default:
             return state;
