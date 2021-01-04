468d02953dfd45c80b9213c5600f0c2d7c1df132
Jue Ning <juening@gmail.com>
2021-01-03 23:23:27 -0500

    createbook and updatebook backend endpoint implemented

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -28,3 +28,60 @@ export const deleteBookById = asyncHandler(async (req, res) => {
         throw new Error('Book not found.')
     }
 })
+
+export const createBook = asyncHandler(async (req, res) => {
+    const newBook = new Book({
+        name:'Test Book',
+        price:1,
+        user:req.user._id,
+        image:'/images/sample.jpg',
+        category:'sample cate',
+        author:'Ted Smith',
+        description:'Test des',
+        numReviews:3,
+        rating:5,
+        price:10,
+        binding:'hardcover',
+        countInStock:7
+    });
+
+    const createdBook = newBook.save();
+
+    res.status(201).json(createBook);
+});
+
+export const updateBook = asyncHandler(async (req, res) => {
+    const {
+        name,
+        price,
+        image,
+        category,
+        author,
+        description,
+        numReviews,
+        rating,
+        binding,
+        countInStock
+    } = req.body;
+
+    const book = Book.findById(req,params.id);
+
+    if(book) {
+        book.name = name || book.name;
+        book.price = price || book.price,
+        book.image = image || book.image;
+        book.category = category || book.category;
+        book.author = author || book.author;
+        book.description = description || book.description;
+        book.numReviews = numReviews || book.numReviews;
+        book.rating= rating || book.rating;
+        book.binding = binding || book.binding;
+        book.countInStock = countInStock || book.countInStock;
+
+        const upatedBook = await book.save();
+        res.json(updatedBook);
+    } else {
+        res.status(404);
+        throw new Error('Book not found.')
+    }
+})
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
-import {getBooks, getBookById, deleteBookById} from '../controllers/bookController.js';
+import {getBooks, getBookById, deleteBookById, createBook, updateBook} from '../controllers/bookController.js';
 import {protect, admin} from '../middleware/authMiddleware.js';
 
 
-router.route('/').get(getBooks)
+router.route('/').get(getBooks).post(createBook)
 
-router.route('/:id').get(getBookById).delete(protect, admin, deleteBookById);
+router.route('/:id').get(getBookById).delete(protect, admin, deleteBookById).put(protect, admin, updateBook);
 
 export default router;
